7ae7b32d713228a68f575a481dcf49b7ce99ebdb
Tommaso Giani <tgiani@stbc-i1.nikhef.nl>
2022-06-29 11:40:57 +0200

    some more work on optimize tests

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -10,45 +10,42 @@ commondata_path = pathlib.Path(__file__).parents[0] / "fake_data"
 # fix the wilson coefficients to 1 and compute the corresponding expected value of the chi2
 
 coeffs_dict = {
-    "Op1": {  # fixed to 1
-        "constrain": True,
-        "value": 1,
+    "Op1": {
         "min": -2,
         "max": 1,
     },
-    "Op2": {  # fixed to 1
-        "constrain": True,
-        "value": 1,
+    "Op2": {
         "min": -2,
         "max": 1,
     },
-    "Op3": {  # fixed to 1
-        "constrain": True,
-        "value": 1,
+    "Op3": {
         "min": -2,
         "max": 1,
     },
-    "Op4": {  # fixed to 1
+    "Op4": {
         "constrain": True,
-        "value": 1,
+        "value": 0.0,
         "min": -2,
         "max": 1,
     },
 }
 
-# theory predictions for dataset1, assuming all the wilson coefficients are 1
+# genaret random values for the wilson coefficients
+wilson_coeff = np.random.rand(3)
+
+# theory predictions for dataset1
 th_pred_1 = (
     np.array(ld.theory_test_1["best_sm"])
-    + np.array(ld.theory_test_1["LO"]["Op1"])
-    + np.array(ld.theory_test_1["LO"]["Op2"])
-    + np.array(ld.theory_test_1["LO"]["Op3"])
-    + np.array(ld.theory_test_1["LO"]["Op1*Op1"])
-    + np.array(ld.theory_test_1["LO"]["Op2*Op2"])
-    + np.array(ld.theory_test_1["LO"]["Op2*Op1"])
-    + np.array(ld.theory_test_1["LO"]["Op2*Op3"])
+    + np.array(ld.theory_test_1["LO"]["Op1"]) * wilson_coeff[0]
+    + np.array(ld.theory_test_1["LO"]["Op2"]) * wilson_coeff[1]
+    + np.array(ld.theory_test_1["LO"]["Op3"]) * wilson_coeff[2]
+    + np.array(ld.theory_test_1["LO"]["Op1*Op1"]) * wilson_coeff[0] ** 2
+    + np.array(ld.theory_test_1["LO"]["Op2*Op2"]) * wilson_coeff[1] ** 2
+    + np.array(ld.theory_test_1["LO"]["Op2*Op1"]) * wilson_coeff[1] * wilson_coeff[0]
+    + np.array(ld.theory_test_1["LO"]["Op2*Op3"]) * wilson_coeff[1] * wilson_coeff[2]
 )
 
-# compute the chi2 for dataset1
+# chi2 for dataset1
 exp_data_1 = np.array(ld.exp_test_1["data"])
 exp_stat_1 = np.array([0.2, 0.3])
 exp_sys_1 = np.array([[0.01, 0.01], [0.02, 0.02]])
@@ -59,16 +56,14 @@ chi2_1 = (
     (exp_data_1 - th_pred_1) @ np.linalg.inv(tot_cov_1) @ (exp_data_1 - th_pred_1).T
 )
 
-# theory predictions for dataset2, assuming all the wilson coefficients are 1
+# theory predictions for dataset2
 th_pred_2 = (
     np.array(ld.theory_test_2["best_sm"])
-    + np.array(ld.theory_test_2["LO"]["Op1"])
-    + np.array(ld.theory_test_2["LO"]["Op2"])
-    + np.array(ld.theory_test_2["LO"]["Op4"])
-    + np.array(ld.theory_test_2["LO"]["Op1*Op1"])
-    + np.array(ld.theory_test_2["LO"]["Op2*Op2"])
-    + np.array(ld.theory_test_2["LO"]["Op2*Op1"])
-    + np.array(ld.theory_test_2["LO"]["Op2*Op4"])
+    + np.array(ld.theory_test_2["LO"]["Op1"]) * wilson_coeff[0]
+    + np.array(ld.theory_test_2["LO"]["Op2"]) * wilson_coeff[1]
+    + np.array(ld.theory_test_2["LO"]["Op1*Op1"]) * wilson_coeff[0] ** 2
+    + np.array(ld.theory_test_2["LO"]["Op2*Op2"]) * wilson_coeff[1] ** 2
+    + np.array(ld.theory_test_2["LO"]["Op2*Op1"]) * wilson_coeff[1] * wilson_coeff[0]
 )
 
 exp_data_2 = np.array(ld.exp_test_2["data"])
@@ -77,7 +72,7 @@ exp_sys_2 = np.array([[0.03, 0.03], [0.04, 0.04], [0.05, 0.05], [0.06, 0.06]])
 exp_cov2 = np.diag(exp_stat_2**2) + exp_sys_2 @ exp_sys_2.T
 tot_cov_2 = exp_cov2 + np.array(ld.theory_test_2["theory_cov"])
 
-# compute the chi2 for dataset2
+# chi2 for dataset2
 chi2_2 = (
     (exp_data_2 - th_pred_2) @ np.linalg.inv(tot_cov_2) @ (exp_data_2 - th_pred_2).T
 )
@@ -110,5 +105,14 @@ class TestOptimize:
             self.test_opt.coefficients.op_name, ["Op1", "Op2", "Op3", "Op4"]
         )
 
-    def test_chi2_func(self):
-        np.testing.assert_allclose(self.test_opt.chi2_func(), chi2_tot, rtol=1e-10)
+    def test_free_parameters(self):
+        np.testing.assert_equal(
+            self.test_opt.free_parameters.op_name, ["Op1", "Op2", "Op3"]
+        )
+
+    def test_chi2_func_ns(self):
+        # set free parameters to random values generated above
+        params = wilson_coeff
+        np.testing.assert_allclose(
+            self.test_opt.chi2_func_ns(params), chi2_tot, rtol=1e-10
+        )
