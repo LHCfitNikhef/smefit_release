193c2eea001d43745abfdca92ace760d06d323b7
Giacomo Magni <giac.magni@gmail.com>
2022-09-14 13:18:26 +0200

    init chi2 scan routines

--- src/smefit/chi2.py
+++ src/smefit/chi2.py
@@ -3,29 +3,37 @@
 """
 Module for the computation of chi-squared values
 """
+import matplotlib.pyplot as plt
 import numpy as np
+from rich.progress import track
+
+import smefit
 
 from . import compute_theory as pr
+from .coefficients import Coefficient, CoefficientManager
+from .loader import DataTuple, load_datasets
+from .log import logging
+
+_logger = logging.getLogger(__name__)
 
 
 def compute_chi2(dataset, coefficients_values, use_quad, use_replica=False):
     r"""
-    Compute the chi2
-    Will need to be modified when implementing training validation split.
+    Compute the :math:`\chi^2`.
 
     Parameters
     ----------
-        dataset : DataTuple
-            dataset tuple
-        coefficients_values : numpy.ndarray
-            |EFT| coefficients values
-        use_quad: bool
-            if True include also |HO| corrections
+    dataset : DataTuple
+        dataset tuple
+    coefficients_values : numpy.ndarray
+        |EFT| coefficients values
+    use_quad: bool
+        if True include also |HO| corrections
 
     Returns
     -------
-        chi2_total : float
-            :math:`\Chi^2` value
+    chi2_total : float
+        :math:`\chi^2` value
     """
 
     # compute theory prediction for each point in the dataset
@@ -40,3 +48,129 @@ def compute_chi2(dataset, coefficients_values, use_quad, use_replica=False):
     invcovmat = dataset.InvCovMat
     # note @ is slower when running with mpiexec
     return np.einsum("i,ij,j->", diff, invcovmat, diff)
+
+
+class Scanner:
+    r"""Class to compute and plot the idividual :math:`\chi^2` scan.
+
+    Parameters
+    ----------
+    run_card : dict
+        run card dictionary
+    n_replica : int
+        number of replica to use
+    """
+
+    def __init__(self, run_card, n_replica):
+
+        self.n_replica = n_replica
+        self.use_quad = run_card["use_quad"]
+        self.result_path = f"{run_card['result_path']}/{run_card['result_ID']}"
+        self.datasets = load_datasets(
+            run_card["data_path"],
+            run_card["datasets"],
+            run_card["coefficients"],
+            run_card["order"],
+            run_card["use_quad"],
+            run_card["use_theory_covmat"],
+            run_card.get("theory_path", None),
+            run_card.get("rot_to_fit_basis", None),
+            run_card.get("uv_coupligs", False),
+        )
+
+        # set all the coefficients to 0
+        self.coefficients = CoefficientManager.from_dict(run_card["coefficients"])
+        self.coefficients.set_free_parameters(
+            np.full(self.coefficients.free_parameters.shape[0], 0)
+        )
+
+        # build empty dict to store results
+        self.chi2_dict = {}
+        for name, row in self.coefficients.free_parameters.iterrows():
+            self.chi2_dict[name] = {}
+            self.chi2_dict[name]["x"] = np.linspace(row.minimum, row.maximum, 100)
+
+    def regularized_chi2_func(self, coeff, xs, use_replica):
+        r"""Individual :math:`\chi^2` wrappper over series of values.
+
+        Parameters
+        ----------
+        coeff: `smefit.coefficient.Coefficient`
+            coefficient to switch on.
+        xs: numpy.array
+            coeffient values.
+        use_replica: bool
+            if True compute the :math:`\chi^2` on |MC| replicas.
+
+        Returns:
+        --------
+        individual reduced :math:`\chi^2` for each x value.
+
+        """
+        chi2_list = []
+        coeff.is_free = True
+        for x in xs:
+            coeff.value = x
+            self.coefficients.set_constraints()
+            chi2_list.append(
+                compute_chi2(
+                    self.datasets,
+                    self.coefficients.value,
+                    self.use_quad,
+                    use_replica,
+                )
+            )
+        return np.array(chi2_list) / self.datasets.Commondata.size
+
+    def run(self):
+        r"""Compute the individual :math:`\chi^2` scan for each replica and coefficient."""
+        # loop on replicas
+        for rep in track(
+            range(self.n_replica + 1),
+            description="[green]Computing chi2 for each replica...",
+        ):
+            use_replica = rep != 0
+            if use_replica:
+                self.datasets = DataTuple(
+                    self.datasets.Commondata,
+                    self.datasets.SMTheory,
+                    self.datasets.OperatorsNames,
+                    self.datasets.LinearCorrections,
+                    self.datasets.QuadraticCorrections,
+                    self.datasets.ExpNames,
+                    self.datasets.NdataExp,
+                    self.datasets.InvCovMat,
+                    np.random.multivariate_normal(
+                        self.datasets.Commondata,
+                        np.linalg.inv(self.datasets.InvCovMat),
+                    ),
+                )
+
+            # loop on coefficients
+            for coeff in self.coefficients:
+                if coeff.name not in self.chi2_dict:
+                    continue
+                self.chi2_dict[coeff.name][rep] = self.regularized_chi2_func(
+                    coeff, self.chi2_dict[coeff.name]["x"], use_replica
+                )
+                coeff.value = 0.0
+
+    def plot_scan(self):
+        r"""Plot and save the :math:`\chi^2` scan for each coefficient."""
+        # loop on coefficients
+        for c, tab in self.chi2_dict.items():
+            _logger.info(f"Plotting scan for {c}")
+            plt.figure()
+            for rep in range(self.n_replica + 1):
+                chi2_min = np.array(tab[rep]).min()
+                if rep == 0:
+                    plt.plot(tab["x"], tab[rep] - chi2_min)
+                else:
+                    plt.plot(
+                        tab["x"], tab[rep] - chi2_min, alpha=0.2, color="lightskyblue"
+                    )
+
+            plt.ylabel(r"$\chi^2 - \chi^2_{min}$")
+            plt.title(f"{c}")
+            plt.tight_layout()
+            plt.savefig(f"{self.result_path}/chi2_scan_{c}.pdf")
