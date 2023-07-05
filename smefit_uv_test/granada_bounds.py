1e58e59f64cd8625e42cd2223b210d641ae11503
jacoterh <j.j.ter.hoeve@vu.nl>
2023-07-05 12:35:16 +0200

    adding latex to granada bounds table

--- smefit_uv_test/granada_bounds.py
+++ smefit_uv_test/granada_bounds.py
@@ -6,13 +6,31 @@ import numpy as np
 from matplotlib import rc, use
 from collections import defaultdict
 
-
 use("PDF")
 rc("font", **{"family": "sans-serif", "sans-serif": ["Helvetica"]})
 rc("text", **{"usetex": True, "latex.preamble": r"\usepackage{amssymb}"})
 
 # Specify the path to the JSON file
 posterior_path = "/data/theorie/jthoeve/smefit_release/results/Granada_UV_{}_{}_{}_NS/posterior.json"
+
+mod_dict = {2: r"$\mathcal{S}$", 3: r"$\mathcal{S}_1$", 5: r"$\varphi$", 6: r"$\Xi$", 7: r"$\Xi_1$", 10: r"$\omega_1$",
+            12: r"$\omega_4$", 15: r"$\zeta$", 16: r"$\Omega_1$", 18: r"$\Omega_4$", 19: r"$\Upsilon$", 20: r"$\Phi$"}
+
+uv_param_dict = {'kS': r"$\kappa_{\mathcal{S}}$",
+                 'yS1f12': r"$\left(y_{\mathcal{S}_1}\right)_{12}$",
+                 'yS1f21': r"$\left(y_{\mathcal{S}_1}\right)_{21}$",
+                 'lamVarphi': r"$\lambda_{\varphi}$",
+                 'yVarphiuf33': r"$\left(y_{\varphi}^u\right)_{33}$",
+                 'kXi': r"$\kappa_{\Xi}$",
+                 'kXi1': r"$\kappa_{\Xi1}$",
+                 'yomega1qqf33': r"$\left(y_{\omega_1}^{qq}\right)_{33}$",
+                 'yomega4uuf33': r"$\left(y_{\omega_4}^{uu}\right)_{33}$",
+                 'yZetaqqf33': r"$\left(y_{\Zeta}^{qq}\right)_{33}$",
+                 'yOmega1qqf33': r"$\left(y_{\Omega_1}^{qq}\right)_{33}$",
+                 'yOmega4f33': r"$\left(y_{\Omega_4}\right)_{33}$",
+                 'yUpsf33': r"$\left(y_{\Upsilon}\right)_{33}$",
+                 'yPhiquf33': r"$\left(y_{\Phi}^{qu}\right)_{33}$"
+                 }
 
 
 def nested_dict(num_levels):
@@ -21,9 +39,10 @@ def nested_dict(num_levels):
     else:
         return defaultdict(lambda: nested_dict(num_levels - 1))
 
+
 table_dict = nested_dict(4)
 
-for mod_nr in range(49):
+for mod_nr in range(21):
     for pQCD in ['LO', 'NLO']:
         for EFT in ['NHO', 'HO']:
             # path to posterior with model number
@@ -41,14 +60,18 @@ for mod_nr in range(49):
                     else:
                         table_dict[mod_nr][key][pQCD][EFT] = [np.percentile(samples, 2.5), np.percentile(samples, 97.5)]
 
+
 def nested_dict_to_latex_table(nested_dict):
     table = "\\documentclass{article}\\n"
     table += "\\usepackage[a4paper]{geometry}\\n"
-    table += "\\geometry{verbose, tmargin = 1.5cm, bmargin = 1.5cm, lmargin = 1cm, rmargin = 1cm}\\n"
+    # table += "\\geometry{verbose, tmargin = 1.5cm, bmargin = 1.5cm, lmargin = 1cm, rmargin = 1cm}\\n"
     table += "\\begin{document}\n"
+    table += "\\begin{table}\n"
+    table += "\\begin{center}\n"
+    table += "\\renewcommand{\\arraystretch}{1.4}"
     table += "\\begin{tabular}{|c|c|c|c|c|c|}\n"
     table += "\\hline\n"
-    table += "Model & Parameter & LO lin & LO quad & NLO lin & NLO quad \\\\\n"
+    table += "Model & UV coupling & LO \\mathcal{O}\\left(\\Lambda^{-2}\\right) &LO \\mathcal{O}\\left(\\Lambda^{-4}\\right) & NLO \\mathcal{O}\\left(\\Lambda^{-2}\\right) & NLO \\mathcal{O}\\left(\\Lambda^{-4}\\right) \\\\\n"
     table += "\\hline\n"
 
     for model_nr, params in nested_dict.items():
@@ -61,16 +84,20 @@ def nested_dict_to_latex_table(nested_dict):
 
             if model_row:
 
-                table += f"{model_nr} & {parameter} & {LO_NHO} & {LO_HO} & {NLO_NHO} & {NLO_HO} \\\\\n"
+                table += f"{mod_dict[model_nr]} & {uv_param_dict[parameter]} & {LO_NHO} & {LO_HO} & {NLO_NHO} & {NLO_HO} \\\\\n"
                 model_row = False
             else:
-                table += f" & {parameter} & {LO_NHO} & {LO_HO} & {NLO_NHO} & {NLO_HO}  \\\\\n"
+                table += f" & {uv_param_dict[parameter]} & {LO_NHO} & {LO_HO} & {NLO_NHO} & {NLO_HO}  \\\\\n"
 
     table += "\\hline\n"
-    table += "\\end{tabular}"
+    table += "\\end{tabular}\n"
+    table += "\\caption{\label{demo-table}95\\% CL intervals on the heavy scalar fields UV couplings.}\n"
+    table += "\\end{center}\n"
+    table += "\\end{table}\n"
     table += "\\end{document}\n"
 
     return table
+
 
 latex_code = nested_dict_to_latex_table(table_dict)
 
@@ -84,4 +111,3 @@ subprocess.run(["rm", "table.aux", "table.log"])
 # Rename the output file
 
 print("PDF file saved as 'output.pdf'.")
-
