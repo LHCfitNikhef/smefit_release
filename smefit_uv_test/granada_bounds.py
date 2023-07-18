176b0c6c8b7223f7029dc001c0e91094ff348fee
jacoterh <j.j.ter.hoeve@vu.nl>
2023-07-18 18:08:50 +0200

    updating x range in histograms

--- smefit_uv_test/granada_bounds.py
+++ smefit_uv_test/granada_bounds.py
@@ -9,13 +9,14 @@ from pathlib import Path
 from latex_dicts import mod_dict
 from latex_dicts import uv_param_dict
 from latex_dicts import inv_param_dict
+import arviz as az
 
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import rc, use
 
 here = pathlib.Path(__file__).parent
-base_path = pathlib.Path(f"{here.parent}/runcards/uv_models/UV_scan/Granada/")
+base_path = pathlib.Path(f"{here.parent}/runcards/uv_models/UV_scan/MP/")
 sys.path = [str(base_path)] + sys.path
 
 # result dir
@@ -77,16 +78,7 @@ def compute_bounds(mod_nrs):
                         if not key.startswith("inv"):
                             continue
                         else:
-                            if np.array(samples).min() > 0:
-                                table_dict[mod_nr][key][pQCD][EFT] = [
-                                    0,
-                                    np.percentile(samples, 95),
-                                ]
-                            else:
-                                table_dict[mod_nr][key][pQCD][EFT] = [
-                                    np.percentile(samples, 2.5),
-                                    np.percentile(samples, 97.5),
-                                ]
+                            table_dict[mod_nr][key][pQCD][EFT] = az.hdi(np.array(samples), hdi_prob=.95)
                             n_params += 1
 
     return table_dict, int(n_params / 4)
@@ -132,10 +124,12 @@ def dict_to_latex_table(nested_dict, mod_dict, uv_param_dict, label, caption):
 scalar_mdl_nrs = range(21)
 vboson_mdl_nrs = range(21, 37)
 vfermion_mdl_nrs = range(37, 50)
+mp_mdl_idx = ["Q1_Q7_W1"]
 
 scalar_dict, n_scalars = compute_bounds(scalar_mdl_nrs)
 vector_boson_dict, n_vbosons = compute_bounds(vboson_mdl_nrs)
 vector_fermion_dict, n_vfermions = compute_bounds(vfermion_mdl_nrs)
+mp_dict, n_mp = compute_bounds(mp_mdl_idx)
 
 latex_table_scalar = dict_to_latex_table(
     scalar_dict,
@@ -158,14 +152,24 @@ latex_table_vfermion = dict_to_latex_table(
     "cl-heavy-vfermion",
     "95\\% CL intervals of the heavy vector fermion fields UV couplings.",
 )
+latex_table_mp = dict_to_latex_table(
+    mp_dict,
+    mod_dict,
+    uv_param_dict,
+    "cl-mp-model",
+    "95\\% CL intervals of the UV couplings that enter in the multiparticle model.",
+)
 
-# Save LaTeX code to a .tex file
+#Save LaTeX code to a .tex file
 with open(result_dir / "table_scalar.tex", "w") as file:
     file.write(latex_table_scalar)
 with open(result_dir / "table_vboson.tex", "w") as file:
     file.write(latex_table_vboson)
 with open(result_dir / "table_vfermion.tex", "w") as file:
     file.write(latex_table_vfermion)
+with open(result_dir / "table_mp.tex", "w") as file:
+    file.write(latex_table_mp)
+
 
 
 # Compile LaTeX code to PDF
@@ -178,11 +182,11 @@ with open(result_dir / "table_vfermion.tex", "w") as file:
 # subprocess.run(["rm", str(result_dir / "table_vfermion.aux"), str(result_dir / "table_vfermion.log")])
 
 
-def plot_uv_posterior(n_params, mod_nrs, EFT, name, pQCD=None):
+def plot_uv_posterior(n_params, mod_nrs, EFT, name=None, pQCD=None):
     plot_nr = 1
 
     # Calculate number of rows and columns
-    n_cols = int(np.ceil(np.sqrt(n_params)))
+    n_cols = max(int(np.ceil(np.sqrt(n_params))), 4)
     n_rows = int(np.ceil(n_params / n_cols))
 
     fig = plt.figure(figsize=(n_cols * 4, n_rows * 4))
@@ -206,17 +210,68 @@ def plot_uv_posterior(n_params, mod_nrs, EFT, name, pQCD=None):
             with open(posterior_path_mod_2) as f:
                 posterior_2 = json.load(f)
 
-            for (key, samples_1), (_, samples_2) in zip(
+            for (key, samples_1_list), (_, samples_2_list) in zip(
                     posterior_1.items(), posterior_2.items()
             ):
                 if not key.startswith("inv"):
                     continue
                 else:
+                    samples_1 = np.array(samples_1_list)
+                    samples_2 = np.array(samples_2_list)
                     ax = plt.subplot(n_rows, n_cols, plot_nr)
                     plot_nr += 1
 
-                    ax.hist(
+                    n_1, bins_1 = np.histogram(
                         samples_1,
+                        bins="fd",
+                        density=True,
+
+                    )
+
+                    n_2, bins_2 = np.histogram(
+                        samples_2,
+                        bins="fd",
+                        density=True,
+
+                    )
+
+                    max_index_1 = np.argmax(n_1)
+                    threshold_1 = 0.05 * n_1[max_index_1]
+                    max_index_2 = np.argmax(n_2)
+                    threshold_2 = 0.05 * n_2[max_index_2]
+                    indices_1 = np.where(n_1 >= threshold_1)[0]
+                    indices_2 = np.where(n_2 >= threshold_2)[0]
+
+                    low_1, up_1 = az.hdi(samples_1, hdi_prob=.95)
+                    low_2, up_2 = az.hdi(samples_2, hdi_prob=.95)
+
+                    min_low = min(low_1, low_2)
+                    max_up = max(up_1, up_2)
+                    delta = 0.1 * (max_up - min_low)
+
+                    index_max_1 = np.max(indices_1)
+                    index_min_1 = np.min(indices_1)
+                    index_max_2 = np.max(indices_2)
+                    index_min_2 = np.min(indices_2)
+
+                    if min_low < 0:
+                        x_low_1 = bins_1[index_min_1]
+                    x_high_1 = bins_1[index_max_1]
+                    if min_low < 0:
+                        x_low_2 = bins_2[index_min_2]
+                    x_high_2 = bins_2[index_max_2]
+
+                    if min_low > 0:
+                        x_low_1 = - 0.1 * x_high_1
+                        x_low_2 = - 0.1 * x_high_2
+                        x_low = min(x_low_1, x_low_2)
+                    else:
+                        x_low = min(x_low_1, x_low_2)
+                    x_high = max(x_high_1, x_high_2)
+
+
+                    ax.hist(
+                        samples_1[(samples_1 > x_low) & (samples_1 < x_high)],
                         bins="fd",
                         density=True,
                         edgecolor="black",
@@ -224,12 +279,15 @@ def plot_uv_posterior(n_params, mod_nrs, EFT, name, pQCD=None):
                     )
 
                     ax.hist(
-                        samples_2,
+                        samples_2[(samples_2 > x_low) & (samples_2 < x_high)],
                         bins="fd",
                         density=True,
                         edgecolor="black",
                         alpha=0.4,
                     )
+
+                    ax.set_xlim(x_low, x_high)
+                    #ax.set_xlim(min_lim - 0.1 * (max_lim - min_lim), max_lim + 0.1 * (max_lim - min_lim))
 
                     ax.set_ylim(0, ax.get_ylim()[1] + ax.get_ylim()[1] * 0.2)
 
@@ -254,16 +312,30 @@ def plot_uv_posterior(n_params, mod_nrs, EFT, name, pQCD=None):
         order_EFT = -2 if EFT == 'NHO' else -4
         fig.legend([f"$\mathrm{{LO}}\;\mathcal{{O}}\\left(\Lambda^{{{order_EFT}}}\\right)$",
                     f"$\mathrm{{NLO}}\;\mathcal{{O}}\\left(\Lambda^{{{order_EFT}}}\\right)$"], loc="upper center",
-                   prop={"size": 35}, ncol=2)
-        fig.savefig(result_dir / "granada_posteriors_LO_vs_NLO_{}_{}.pdf".format(EFT, name))
+                   prop={"size": 20}, ncol=2)
+        plt.subplots_adjust(top=0.90, wspace=0.1, hspace=0.15)
+
+        if name is not None:
+            fig.savefig(result_dir / "granada_posteriors_LO_vs_NLO_{}_{}.pdf".format(EFT, name))
+        else:
+            fig.savefig(result_dir / "granada_posteriors_LO_vs_NLO_{}.pdf".format(EFT))
     else:
         fig.legend([f"$\mathrm{{{pQCD}}}\;\mathcal{{O}}\\left(\Lambda^{{-2}}\\right)$",
                     f"$\mathrm{{{pQCD}}}\;\mathcal{{O}}\\left(\Lambda^{{-4}}\\right)$"], loc="upper center", ncol=2,
-                   prop={"size": 35})
-        fig.savefig(result_dir / "granada_posteriors_HO_vs_NHO_{}_{}.pdf".format(pQCD, name))
+                   prop={"size": 20})
+        plt.subplots_adjust(top=0.90, wspace=0.1, hspace=0.1)
+        if name is not None:
+            fig.savefig(result_dir / "granada_posteriors_HO_vs_NHO_{}_{}.pdf".format(pQCD, name))
+        else:
+            fig.savefig(result_dir / "granada_posteriors_HO_vs_NHO_{}.pdf".format(pQCD))
 
-    plt.tight_layout()
 
+
+
+# plot_uv_posterior(n_mp, mp_mdl_idx, "NHO")
+# plot_uv_posterior(n_mp, mp_mdl_idx, "HO")
+# plot_uv_posterior(n_mp, mp_mdl_idx, "NHO", pQCD="LO")
+# plot_uv_posterior(n_mp, mp_mdl_idx, "HO", pQCD="NLO")
 
 plot_uv_posterior(n_vbosons, vboson_mdl_nrs, "NHO", "vbosons")
 plot_uv_posterior(n_vbosons, vboson_mdl_nrs, "HO", "vbosons")
@@ -278,7 +350,7 @@ plot_uv_posterior(n_vfermions, vfermion_mdl_nrs, "NHO", "vfermions", pQCD="LO")
 plot_uv_posterior(n_vfermions, vfermion_mdl_nrs, "HO", "vfermions", pQCD="NLO")
 plot_uv_posterior(n_scalars, scalar_mdl_nrs, "NHO", "scalars", pQCD="LO")
 plot_uv_posterior(n_scalars, scalar_mdl_nrs, "HO", "scalars", pQCD="NLO")
-
+#
 for file in result_dir.iterdir():
     if file.suffix == ".pdf":
         subprocess.run(["pdfcrop", str(file), str(file)])
