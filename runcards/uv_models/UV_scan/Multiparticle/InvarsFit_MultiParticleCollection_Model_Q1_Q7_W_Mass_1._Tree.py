2e3b56a4bf2656b19635f65a3fbac06b939f3a38
Alejo Rossia <alejo.r94@gmail.com>
2023-07-25 17:14:49 +0100

    Updated multiparticle model.
    Updated with UV couplings fulfilling the SMEFiT UV assumption. Changed name to reflect that the W is the \mathcal{W}, not \mathcal{W}_1

---
+++ runcards/uv_models/UV_scan/Multiparticle/InvarsFit_MultiParticleCollection_Model_Q1_Q7_W_Mass_1._Tree.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from utils import inspect_model
+
+
+MODEL_SPECS = dict(
+	 id= "Q1_Q7_W1",
+	 collection= "MultiParticleCollection",
+	 mass=1., # in TeV
+	 pto="NLO",
+	 eft="NHO"
+)
+
+
+def inv1(results):
+	gWLf11 = results.gWLf11
+	return np.abs(gWLf11)
+
+def inv2(results):
+	gWLf11 = results.gWLf11
+	gWLf22 = results.gWLf22
+	return (gWLf11*gWLf22)/np.abs(gWLf11)
+
+def inv3(results):
+	gWLf33 = results.gWLf33
+	return np.abs(gWLf33)
+
+def inv4(results):
+	gWqf33 = results.gWqf33
+	return np.abs(gWqf33)
+
+def inv5(results):
+	gWqf33 = results.gWqf33
+	gWtiH = results.gWtiH
+	return (gWqf33*gWtiH)/np.abs(gWqf33)
+
+def inv6(results):
+	lamQ1uf3 = results.lamQ1uf3
+	return np.abs(lamQ1uf3)
+
+def inv7(results):
+	lamQ7f33 = results.lamQ7f33
+	return np.abs(lamQ7f33)
+
