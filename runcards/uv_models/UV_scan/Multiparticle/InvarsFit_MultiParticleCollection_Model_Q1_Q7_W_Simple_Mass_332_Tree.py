5f36251b971a34ac7fbf401393fd3af604ba3208
Alejo Rossia <alejo.r94@gmail.com>
2023-07-27 14:17:10 +0100

    Multiparticle simplified.
    Added version of the multiparticle model with simplified couplings. \mathcal{W} only couples to the Higgs. Q1 and Q7 have the same masses and the same couplings to the top.

---
+++ runcards/uv_models/UV_scan/Multiparticle/InvarsFit_MultiParticleCollection_Model_Q1_Q7_W_Simple_Mass_332_Tree.py
@@ -0,0 +1,14 @@
+import numpy as np
+from utils import inspect_model
+
+MODEL_SPECS = dict(id="Q1_Q7_W_Simple", collection="MultiParticleCollection", mass=332, pto="NLO", eft="NHO" )
+
+
+def inv1(results):
+	gWtiH = results.gWtiH
+	return np.abs(gWtiH)
+
+def inv2(results):
+	lamQuf = results.lamQuf
+	return np.abs(lamQuf)
+
