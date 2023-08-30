c1e1ca41ddbf11e7bb04264dbe6b226a53dafce2
Alejo Rossia <alejo.r94@gmail.com>
2023-08-30 09:27:54 +0100

    Updated multiparticle model
    Updated with flavour assumption for W according to EWPO constrain on cll

--- runcards/uv_models/UV_scan/Multiparticle/InvarsFit_MultiParticleCollection_Model_Q1_Q7_W_Mass_1_Tree.py
+++ runcards/uv_models/UV_scan/Multiparticle/InvarsFit_MultiParticleCollection_Model_Q1_Q7_W_Mass_1_Tree.py
@@ -9,28 +9,23 @@ def inv1(results):
 	return np.abs(gWLf11)
 
 def inv2(results):
-	gWLf11 = results.gWLf11
-	gWLf22 = results.gWLf22
-	return (gWLf11*gWLf22)/np.abs(gWLf11)
-
-def inv3(results):
 	gWLf33 = results.gWLf33
 	return np.abs(gWLf33)
 
-def inv4(results):
+def inv3(results):
 	gWqf33 = results.gWqf33
 	return np.abs(gWqf33)
 
-def inv5(results):
+def inv4(results):
 	gWqf33 = results.gWqf33
 	gWtiH = results.gWtiH
 	return (gWqf33*gWtiH)/np.abs(gWqf33)
 
-def inv6(results):
+def inv5(results):
 	lamQ1uf3 = results.lamQ1uf3
 	return np.abs(lamQ1uf3)
 
-def inv7(results):
+def inv6(results):
 	lamQ7f33 = results.lamQ7f33
 	return np.abs(lamQ7f33)
 
