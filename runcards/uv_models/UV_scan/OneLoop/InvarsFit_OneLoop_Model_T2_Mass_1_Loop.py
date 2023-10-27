f04ca5cdf22df905e5042bce982085582a97bf83
Alejo Rossia <alejo.r94@gmail.com>
2023-10-27 11:16:26 +0100

    Added fermions at 1 loop
    Add run cards for fermion models at 1 loop, T1 and T2. Invariants computed from tree-level matching expressions.

---
+++ runcards/uv_models/UV_scan/OneLoop/InvarsFit_OneLoop_Model_T2_Mass_1_Loop.py
@@ -0,0 +1,10 @@
+import numpy as np
+from utils import inspect_model
+
+MODEL_SPECS = dict(id="T2", collection="OneLoop", mass=1, pto="NLO", eft="NHO" )
+
+
+def inv1(results):
+	lambdaT23 = results.lambdaT23
+	return np.abs(lambdaT23)
+
