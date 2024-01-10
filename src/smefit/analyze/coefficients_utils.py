bfe10efd98a98f9b4a05bc99f9a503820cd97930
jacoterh <j.j.ter.hoeve@vu.nl>
2024-01-10 11:25:13 +0100

    more work on spider plot

--- src/smefit/analyze/coefficients_utils.py
+++ src/smefit/analyze/coefficients_utils.py
@@ -7,13 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib import cm
+from .spider import radar_factory
 
-from matplotlib.patches import Circle, RegularPolygon
-from matplotlib.path import Path
-from matplotlib.projections import register_projection
-from matplotlib.projections.polar import PolarAxes
-from matplotlib.spines import Spine
-from matplotlib.transforms import Affine2D
 
 from .contours_2d import plot_contours
 from .latex_tools import latex_packages, multicolum_table_header
@@ -102,6 +97,7 @@ def compute_confidence_level(
                 bounds[(group, latex_name)] = pd.DataFrame(
                     [get_confidence_values(posterior[name], has_posterior)]
                 ).stack()
+
     return pd.DataFrame(bounds)
 
 
@@ -318,95 +314,7 @@ class CoefficientsPlotter:
         plt.savefig(f"{self.report_folder}/coefficient_bar.pdf", dpi=500)
         plt.savefig(f"{self.report_folder}/coefficient_bar.png")
 
-    @staticmethod
-    def radar_factory(num_vars, frame='circle'):
-        """
-        Create a radar chart with `num_vars` axes.
-
-        This function creates a RadarAxes projection and registers it.
-
-        Parameters
-        ----------
-        num_vars : int
-            Number of variables for radar chart.
-        frame : {'circle', 'polygon'}
-            Shape of frame surrounding axes.
-
-        """
-        # calculate evenly-spaced axis angles
-        theta = np.linspace(0, 2 * np.pi, num_vars, endpoint=False)
-
-        class RadarTransform(PolarAxes.PolarTransform):
-
-            def transform_path_non_affine(self, path):
-                # Paths with non-unit interpolation steps correspond to gridlines,
-                # in which case we force interpolation (to defeat PolarTransform's
-                # autoconversion to circular arcs).
-                if path._interpolation_steps > 1:
-                    path = path.interpolated(num_vars)
-                return Path(self.transform(path.vertices), path.codes)
-
-        class RadarAxes(PolarAxes):
-
-            name = 'radar'
-            PolarTransform = RadarTransform
-
-            def __init__(self, *args, **kwargs):
-                super().__init__(*args, **kwargs)
-                # rotate plot such that the first axis is at the top
-                self.set_theta_zero_location('N')
-
-            def fill(self, *args, closed=True, **kwargs):
-                """Override fill so that line is closed by default"""
-                return super().fill(closed=closed, *args, **kwargs)
-
-            def plot(self, *args, **kwargs):
-                """Override plot so that line is closed by default"""
-                lines = super().plot(*args, **kwargs)
-                for line in lines:
-                    self._close_line(line)
-
-            def _close_line(self, line):
-                x, y = line.get_data()
-                # FIXME: markers at x[0], y[0] get doubled-up
-                if x[0] != x[-1]:
-                    x = np.append(x, x[0])
-                    y = np.append(y, y[0])
-                    line.set_data(x, y)
-
-            def set_varlabels(self, labels):
-                self.set_thetagrids(np.degrees(theta), labels)
-
-            def _gen_axes_patch(self):
-                # The Axes patch must be centered at (0.5, 0.5) and of radius 0.5
-                # in axes coordinates.
-                if frame == 'circle':
-                    return Circle((0.5, 0.5), 0.5)
-                elif frame == 'polygon':
-                    return RegularPolygon((0.5, 0.5), num_vars,
-                                          radius=.5, edgecolor="k")
-                else:
-                    raise ValueError("Unknown value for 'frame': %s" % frame)
-
-            def _gen_axes_spines(self):
-                if frame == 'circle':
-                    return super()._gen_axes_spines()
-                elif frame == 'polygon':
-                    # spine_type must be 'left'/'right'/'top'/'bottom'/'circle'.
-                    spine = Spine(axes=self,
-                                  spine_type='circle',
-                                  path=Path.unit_regular_polygon(num_vars))
-                    # unit_regular_polygon gives a polygon of radius 1 centered at
-                    # (0, 0) but we want a polygon of radius 0.5 centered at (0.5,
-                    # 0.5) in axes coordinates.
-                    spine.set_transform(Affine2D().scale(.5).translate(.5, .5)
-                                        + self.transAxes)
-                    return {'polar': spine}
-                else:
-                    raise ValueError("Unknown value for 'frame': %s" % frame)
-
-        register_projection(RadarAxes)
-        return theta
+    
 
     def plot_spider(
             self,
@@ -426,37 +334,52 @@ class CoefficientsPlotter:
             error: dict
                confidence level bounds per fit and coefficient
         """
+        def cart2pol(x, y):
+            rho = np.sqrt(x**2 + y**2)
+            phi = np.arctan2(y, x)
+            return rho, phi
+
+
         df = pd.DataFrame(error)
+        
+        # check if more than one fit is loaded
+        if df.shape[1] < 2:
+            print("At least two fits are required for the spider plot atm")
+        
 
-        N = 45
-        theta = self.radar_factory(N, frame='polygon')
+        theta = radar_factory(len(df), frame='circle')
 
-        data = df.iloc[:, 0].values
+        # normalise to first fit
+        data = df.iloc[:, 1].values / df.iloc[:, 0].values
+
+        x = data * np.cos(theta)
+        y = data * np.sin(theta)
+        x = np.r_[x]
+        y = np.r_[y]
+
+        from scipy import interpolate
+        tck, u = interpolate.splprep([x,y], s=0, per=False)
+        xi, yi = interpolate.splev(np.linspace(0, 1, 1000), tck)
+
         spoke_labels = [op[1] for op in df.index]
 
         fig, ax = plt.subplots(figsize=(9, 9),
                                 subplot_kw=dict(projection='radar'))
         fig.subplots_adjust(wspace=0.25, hspace=0.20, top=0.85, bottom=0.05)
 
-
-        # Plot the four cases from the example data on separate axes
-  
         ax.set_rgrids([0.2, 0.4, 0.6, 0.8])
-        # ax.set_title(title, weight='bold', size='medium', position=(0.5, 1.1),
-        #              horizontalalignment='center', verticalalignment='center')
 
-        ax.plot(theta, data, color='C0')
-        ax.fill(theta, data, facecolor='C0', alpha=0.25, label='_nolegend_')
+        ax.plot(theta, data, color='gold', label=r'$\mathrm{LHC + FCCee + OO(WW),\:NLO}\:\mathcal{O}\left(\Lambda^{-2}\right)$')
+        #ax.plot(cart2pol(xi, yi)[1], cart2pol(xi,yi)[0], color='C1', label='smooth')
+        ax.scatter(theta, data, color='gold', marker='*', s=140)
+        ax.fill(theta, data, facecolor='gold', alpha=0.25, label='_nolegend_')
         ax.set_varlabels(spoke_labels)
+        ax.set_title(r'$\mathrm{Relative\:improvement}$')
 
-        # add legend relative to top-left plot
-        labels = ('Factor 1')
-        legend = ax.legend(labels, loc=(0.9, .95),
-                                  labelspacing=0.1, fontsize='small')
+        #plt.legend(frameon=False, loc='upper right')
 
-        fig.text(0.5, 0.965, '5-Factor Solution Profiles Across Four Scenarios',
-                 horizontalalignment='center', color='black', weight='bold',
-                 size='large')
+
+        ax.legend(loc=(0, 1), labelspacing=0, fontsize='small', frameon=False)
 
         plt.tight_layout()
         plt.savefig(f"{self.report_folder}/spider_plot.pdf", dpi=500)
