a5d29c8d872640b845f8bae0c3494edf4f4f5065
Alejo Rossia <alejo.r94@gmail.com>
2024-02-12 10:33:35 +0000

    Updated OOs for FCCee/CEPC
    Added all the inverse covariances for FCCee and CEPC, leptonic and semileptonic channels, 240 and 365 GeV. Updated runner script. 161 GeV data to be added soon.

--- add_optimal_chi2/interface_oos.py
+++ add_optimal_chi2/interface_oos.py
@@ -1,29 +1,95 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import pathlib
+
+from smefit.runner import Runner
+from smefit.chi2 import Scanner
+import smefit.log as log
+from smefit.log import print_banner, setup_console
+from smefit.optimize.ultranest import USOptimizer
 
 
-def make_oos_chi2(coefficients):
 
-    # #base of WCs in the oos
+def make_oos_likelihood(name:str, smeft_ops:list[str]):
+    #base of WCs in the oos
+    oo_wc_basis=['OpD','OpWB','OWWW','Opl1','Ope','O3pl1']
+    #Check if any of the operators in the oos are being fitted.
+    check_fit=any(b in smeft_ops for b in oo_wc_basis)
+    if not check_fit:
+        return None
+    #Use this to read the inverse covariance exported from Mathematica
+    LIKELIHOODS = {
+        'FCC_ee_ww_lepto_240': {'file':'invcov_FCCee_ww_leptonic_240.dat'},
+        'FCC_ee_ww_lepto_365': {'file':'invcov_FCCee_ww_leptonic_365.dat'},
+        'FCC_ee_ww_semilep_240': {'file':'invcov_FCCee_ww_semilep_240.dat'},
+        'FCC_ee_ww_semilep_365': {'file':'invcov_FCCee_ww_semilep_365.dat'},
+        'CEPC_ee_ww_lepto_240': {'file':'invcov_CEPC_ww_leptonic_240.dat'},
+        'CEPC_ee_ww_lepto_365': {'file':'invcov_CEPC_ww_leptonic_365.dat'},
+        'CEPC_ee_ww_semilep_240': {'file':'invcov_CEPC_ww_semilep_240.dat'},
+        'CEPC_ee_ww_semilep_365': {'file':'invcov_CEPC_ww_semilep_365.dat'}
+    }
 
-    oo_wc_basis = ['OpD', 'OpWB', 'OWWW', 'Opl1', 'Ope', 'O3pl1']
+    if name not in LIKELIHOODS.keys():
+        return None
 
-    project = np.zeros((len(oo_wc_basis), coefficients.size))
+    #import inverse covariance as 6x6 np.array
+    invcov = np.loadtxt(LIKELIHOODS[name]['file'])
+    #Projector from one basis to the other.
+    project=np.zeros((len(oo_wc_basis),len(smeft_ops)))
     for i, op in enumerate(oo_wc_basis):
-        if op in coefficients.name:
-            project[i, np.argwhere(coefficients.name == op)[0, 0]] = 1
+        if op in smeft_ops:
+            project[i,smeft_ops.index(op)]=1
+    proj_transp=np.transpose(project)
+
+    return lambda wc: np.linalg.multi_dot([w,proj_transp,invcov,project,w])
 
 
-    datasets = {'FCC_ee_ww_lepto_240': '/data/theorie/jthoeve/smefit_release/add_optimal_chi2/invcov_FCC_ee_ww_leptonic_240.dat',
-                'FCC_ee_ww_lepto_365': '/data/theorie/jthoeve/smefit_release/add_optimal_chi2/invcov_FCC_ee_ww_leptonic_365.dat'
-                }
-    chi2 = 0
-    n_dat = len(oo_wc_basis)
+def run_smefit(smefit_runcard, log_path):
+    
+    '''
+    Parameters
+    ----------
+    smefit_runcard: pathlib.Path
+        Path to smefit runcard
+    log_path: pathlib.Path
+        Path to log
+    external_chi2: function
+        External likelihood to add the SMEFiT likelihood
+    '''
+    #set up log
+    setup_console(log_path)
+    print_banner()
 
-    for dataset, invcov_path in datasets.items():
-        # #import inverse covariance as 6x6 np.array
-        invcov = np.loadtxt(invcov_path)
-        chi2 += np.linalg.multi_dot([coefficients.value, project.T, invcov, project, coefficients.value])
+    #link SMEFiT runcard
+    runner = Runner.from_file(smefit_runcard.absolute())
+    log.console.log("Running: Nested Sampling fit")
 
-    return chi2, n_dat
+    #check which operators are being fitted
+    fitops= runner.run_card['coefficients'].keys()
+    smeftops=[]
+    for i, op in enumerate(fitops):
+        smeftops.append(op) 
+    #Get the optimal observables likelihood. Add further lines for other channels and energies
+    #FCC-ee likelihoods
+    smeft_chi2_fcc_lep_240 = make_oos_likelihood('FCC_ee_ww_lepto_240', smeftops)
+    smeft_chi2_fcc_lep_365 = make_oos_likelihood('FCC_ee_ww_lepto_365', smeftops)
+    smeft_chi2_fcc_semi_240 = make_oos_likelihood('FCC_ee_ww_semilep_240', smeftops)
+    smeft_chi2_fcc_semi_365 = make_oos_likelihood('FCC_ee_ww_semilep_365', smeftops)
+    smeft_chi2_fcc_tot = smeft_chi2_fcc_lep_240+smeft_chi2_fcc_lep_365+smeft_chi2_fcc_semi_240+smeft_chi2_fcc_semi_365
+    #CEPC likelihoods
+    smeft_chi2_cepc_lep_240 = make_oos_likelihood('CEPC_ee_ww_lepto_240', smeftops)
+    smeft_chi2_cepc_lep_365 = make_oos_likelihood('CEPC_ee_ww_lepto_365', smeftops)
+    smeft_chi2_cepc_semi_240 = make_oos_likelihood('CEPC_ee_ww_semilep_240', smeftops)
+    smeft_chi2_cepc_semi_365 = make_oos_likelihood('CEPC_ee_ww_semilep_365', smeftops)
+    smeft_chi2_cepc_tot = smeft_chi2_cepc_lep_240+smeft_chi2_cepc_lep_365+smeft_chi2_cepc_semi_240+smeft_chi2_cepc_semi_365
 
+    # set up the optimizer and link the optimal observables likelihood
+    opt = USOptimizer.from_dict(runner.run_card)
+    opt.external_chi2 = smeft_chi2_fcc_tot + smeft_chi2_cepc_tot
 
+    #start fit
+    opt.run_sampling()
+
+# run the fit
+run_smefit(smefit_runcard=pathlib.Path("smefit_oo_test_runcard.yaml"),log_path=pathlib.Path("smefit_oo_test.log"))
