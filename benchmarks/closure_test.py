3bc45db0fa7510c438e80e589a334f97aafb7dee
jacoterh <j.j.ter.hoeve@vu.nl>
2024-03-13 10:45:17 +0100

    updating projection runcard to include 4t

--- benchmarks/closure_test.py
+++ benchmarks/closure_test.py
@@ -1,13 +1,13 @@
 import subprocess
-import smefit
 
-projection_runcard = '/Users/jaco/Documents/smefit_release/runcards/projections/closure_baseline.yaml'
-fit_runcard = '/Users/jaco/Documents/smefit_release/runcards/A_smefit_baseline_glob_NLO_NHO.yaml'
+smefit = "/project/theorie/jthoeve/miniconda3/envs/smefit/bin/smefit"
+projection_runcard = '/data/theorie/jthoeve/smefit_release/benchmarks/closure.yaml'
+fit_runcard = '/data/theorie/jthoeve/smefit_release/benchmarks/A_smefit_baseline_glob_NLO_NHO.yaml'
 
-n_exp = 1
+n_exp = 5000
 for i in range(n_exp):
     # run projection module
-    subprocess.run(['smefit', 'PROJ', '--closure', projection_runcard])
+    subprocess.run([smefit, 'PROJ', '--closure', projection_runcard])
 
     # run analytic module without samples
-    subprocess.run(['smefit', 'A', fit_runcard])
+    subprocess.run([smefit, 'A', fit_runcard])
