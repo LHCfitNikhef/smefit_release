2e28da6c98d9a20e5422589a0965a3f5e3ac36e8
jacoterh <j.j.ter.hoeve@vu.nl>
2025-02-10 13:15:34 +0100

    adding external chi2 to scan module

--- src/smefit/runner.py
+++ src/smefit/runner.py
@@ -13,7 +13,7 @@ from .chi2 import Scanner
 from .log import logging
 from .optimize.analytic import ALOptimizer
 from .optimize.mc import MCOptimizer
-from .optimize.ultranest import USOptimizer
+from .optimize import Optimizer
 
 try:
     from mpi4py import MPI
@@ -304,6 +304,18 @@ class Runner:
         compute_bounds: bool
             if True compute and save the :math:`\chi^2` bounds.
         """
+
+        if "external_chi2" in self.run_card:
+            external_chi2 = self.run_card["external_chi2"]
+            for _, module in external_chi2.items():
+                module_path = module["path"]
+                path = pathlib.Path(module_path)
+                base_path, stem = path.parent, path.stem
+                if not base_path.exists():
+                    raise FileNotFoundError(f"Path {base_path} does not exist. Modify the runcard and rerun. Exiting")
+                else:
+                    sys.path = [str(base_path)] + sys.path
+
         scan = Scanner(self.run_card, n_replica, scan_points)
         if compute_bounds:
             scan.compute_bounds()
