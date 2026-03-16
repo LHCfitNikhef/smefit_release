276c45461c9ce65c00a972ba12eaa34e36200796
Elie Hammou <elie.hammouvictoroff@gmail.com>
2026-03-16 14:30:30 +0100

    Moved from pseudocode to skeleton implementation

--- src/smefit/external_chi2_smelli.py
+++ src/smefit/external_chi2_smelli.py
@@ -1,50 +1,81 @@
-from smefit.rge import RGE
+"""External χ² interface for smelli."""
+
+from __future__ import annotations
+
+from typing import Iterable, List
+
+import numpy as np
+
+from smefit.rge.rge import RGE
 
 
-class smelli_chi2:
-    # coefficients are mandatory, taken from smefit runcard. rge_dict is the rge dictionary in the runcard,
-    # in this case we will only need it to read the initial scale, but we also need to build the RGE object.
-    # everything else is optional and can be specified in the external_chi2 runcard
-    def __init__(self, coefficients, rge_dict=None, dataset=[]):
-        # this gives you the scale
-        self.scale = rge_dict["init_scale"]
+class SmelliChi2:
+    """Compute a smelli likelihood contribution in a given SMEFiT fit."""
 
-        # this gives you the names of the coeff in smefit
-        self.names = coefficients.name
+    def __init__(
+        self,
+        coefficients,
+        rge_dict=None,
+        include_likelihoods: Iterable[str] | None = None,
+        initial_scale: float | None = None,
+    ):
+        self.names = list(coefficients.name)
 
-        # construct the RGE object. We don't actually need the RGE from smefit, but
-        # it has inside of it a method to translate coefficients from smefit to wcxf.
-        # Maybe not super ideal that it's inside that but ok for now.
-        self.rge_obj = RGE(
-            self.names,
-            self.scale,
-            rge_dict["smeft_accuracy"],
-            rge_dict["adm_QCD"],
-            rge_dict["yukawa"],
-            # the last three are not actually needed
+        # Build smelli objects lazily to avoid hard import side effects.
+        from smelli import GlobalLikelihood
+        from wilson import Wilson
+
+        self._Wilson = Wilson
+        self.include_likelihoods = (
+            ["likelihood_ewpt.yaml"]
+            if include_likelihoods is None
+            else list(include_likelihoods)
         )
 
-        # This is a dictionary of dictionaries, associating to each of the smefit wc names, the wc_dict for smelli
-        self.translation_basis = self.rge_obj.RGEbasis
+        # Initial scale where SMEFT coefficients are defined.
+        if rge_dict is not None and "init_scale" in rge_dict:
+            self.scale = (
+                float(initial_scale)
+                if initial_scale is not None
+                else float(rge_dict.get("init_scale", 1e3))
+            )
+
+            # Translate SMEFiT basis to Warsaw basis.
+            self.translation_basis = RGE(
+                self.names,
+                self.scale,
+                rge_dict.get("smeft_accuracy", "integrate"),
+                rge_dict.get("adm_QCD", False),
+                rge_dict.get("yukawa", "top"),
+            ).RGEbasis
+        else:
+            self.scale = float(initial_scale) if initial_scale is not None else 1e3
+            self.translation_basis = {op: {op: 1.0} for op in self.names}
+
+        self.gl = GlobalLikelihood(
+            eft="SMEFT", basis="Warsaw", include_likelihoods=self.include_likelihoods
+        )
+
+    def _build_wilson_dict(self, coefficient_values):
+        coeff_values = np.asarray(coefficient_values, dtype=float)
+        if coeff_values.shape[0] != len(self.names):
+            raise ValueError(
+                "Coefficient vector length does not match fitted coefficients "
+                f"({coeff_values.shape[0]} != {len(self.names)})."
+            )
+
+        wc_dict = {}
+        for op, c in zip(self.names, coeff_values):
+            for key, val in self.translation_basis[op].items():
+                wc_dict[key] = wc_dict.get(key, 0.0) + float(val) * c
+        return {k: v for k, v in wc_dict.items() if v != 0.0}
 
     def compute_chi2(self, coefficient_values):
-        # this receives an array of numbers, which order corresponds
-        # to self.names
+        wc_dict = self._build_wilson_dict(coefficient_values)
+        w = self._Wilson(wc_dict, self.scale, eft="SMEFT", basis="Warsaw")
+        pt = self.gl.parameter_point(w)
+        return -2.0 * float(pt.log_likelihood_global())
 
-        # you need to take those values and construct the wc_dict for smelli
-        # you have already the basis, which is telling you stuff like
-        # self.translation_basis["OpD"] -> {"phiD": 1e-6}
-        # so you need to take them all, multiply their values for the coefficien_values
-        # then create a single dictionary (possible repeated keys should be summed)
-        # something along these lines (adapted to the situation, this is copied from rge.RGEevolve)
-        wc_dict = {}
-        for op, values in self.RGEbasis.items():
-            for key in values:
-                if key not in wc_wilson:
-                    wc_dict[key] = values[key] * wcs[op]
-                else:
-                    wc_dict[key] += values[key] * wcs[op]
 
-        # now you should have the self.scale and the wc_dict needed for the smelli likelihood.
-        # make sure you are returning a "chi2"
-        return -2 smelli_loglikelihood
+# Backward-compatible class name for older runcards.
+smelli_chi2 = SmelliChi2
